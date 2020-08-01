3c6bcc90e5ac79470ff020abc7b59f960f0f2eb3
Leonardo Severo <leopedroso45@gmail.com>
2020-08-01 20:55:55 -0300

    Preparing for delivery, removing some lint problems.

--- controller/handler/transaction.go
+++ controller/handler/transaction.go
@@ -2,21 +2,29 @@ package handler
 
 import (
 	"encoding/json"
-	"github.com/leopedroso45/UpnidChallenge/controller"
 	"github.com/leopedroso45/UpnidChallenge/domain"
 	"io/ioutil"
 	"net/http"
 )
 
+/*GetTransactions is the "/v1.0/transactions" handler that returns a json response if no error occurs*/
 func GetTransactions(w http.ResponseWriter, r *http.Request) {
 	var transactionsJson []domain.Transaction
 	transactionsBytes, err := ioutil.ReadAll(r.Body)
-	controller.CheckError(err)
-	err = json.Unmarshal(transactionsBytes, &transactionsJson)
-	controller.CheckError(err)
+	if err != nil {
+		http.Error(w, "Error trying to read the request body, check if everything is correct.", 400)
+	}
+	err2 := json.Unmarshal(transactionsBytes, &transactionsJson)
+	if err2 != nil {
+		http.Error(w, "Error trying to read the request body, check if everything is correct.\n"+err2.Error(), 400)
+	}
 	resultSlice := domain.CheckFraud(transactionsJson)
-	resultJson, err := json.Marshal(resultSlice)
-	controller.CheckError(err)
+	resultJson, err3 := json.Marshal(resultSlice)
+	if err3 != nil {
+		http.Error(w, err3.Error(), 500)
+	}
 
-	_, err = w.Write(resultJson)
+	if err2 == nil && err3 == nil {
+		_, err = w.Write(resultJson)
+	}
 }
